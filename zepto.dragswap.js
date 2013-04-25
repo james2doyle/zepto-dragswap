2670007ef6148b8a62ab62cc2718008131a29f19
james2doyle <james2doyle@gmail.com>
2013-04-25 14:22:01 -0400

    Removed .attr dependecies. Moved functions out of return loop. Fixed moving class removal and customization.

--- zepto.dragswap.js
+++ zepto.dragswap.js
@@ -1,6 +1,7 @@
 /*!
  * Zepto HTML5 Drag and Drop Sortable
- * Author: @james2doyle james2doyle@gmail.com
+ * Author: James Doyle(@james2doyle) http://ohdoylerules.com
+ * Repository: https://github.com/james2doyle/zepto-dragswap
  * Licensed under the MIT license
  */
  ;(function($) {
@@ -16,8 +17,8 @@
         'animation': 'animation'
       };
       document.body.insertBefore(el, null);
-      for(var t in transforms) {
-        if(el.style[t] !== undefined) {
+      for (var t in transforms) {
+        if (el.style[t] !== undefined) {
           el.style[t] = "translate3d(1px,1px,1px)";
           getPre = window.getComputedStyle(el).getPropertyValue(transforms[t]);
           // return the successful prefix
@@ -40,11 +41,12 @@
     };
 
     function onAnimEnd(elem) {
-      $(elem).addClass(settings.dropClass);
+      var $elem = $(elem);
+      $elem.addClass(settings.dropClass);
       // add an event for when the animation has finished
-      $(elem).on(settings.prefix + 'End', function() {
+      $elem.on(settings.prefix + 'End', function() {
         // remove the class now that the animation is done
-        $(elem).removeClass(settings.dropClass);
+        $elem.removeClass(settings.dropClass);
       }, false);
     }
 
@@ -65,63 +67,72 @@
     function handleDragLeave(e) {
       $(this).removeClass(settings.overClass); // this / e.target is previous target element.
     }
-    function handleDragOver(e) {
-      if(e.preventDefault) {
-          e.preventDefault(); // Necessary. Allows us to drop.
-        }
-        e.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
-        return false;
-      }
-      var settings = $.extend({}, this.defaultOptions, options);
-      var method = String(options);
-      if (/^(toArray|toJSON)$/.test(method)) {
-        if (method == 'toArray') {
-          var items = [];
-          $(this).find(settings.element).each(function(index, elem) {
-            items.push($(this).attr('id'));
-          });
-          return items;
-        } else if (method == 'toJSON') {
-          var items = [];
-          $(this).find(settings.element).each(function(index, elem) {
-            // items[index] = {};
-            items[index] = { id: $(this).attr('id') };
-          });
-          return JSON.stringify(items);
-        }
-        return;
-      }
-      return this.each(function(index, item) {
-        var $this = $(this);
-        // select all but the disabled things
-        var $elem = $this.find(settings.element).filter(':not(' + settings.exclude + ')');
 
-        function handleDrop(e) {
-        // this / e.target is current target element.
-        if(e.stopPropagation) {
-          e.stopPropagation(); // Stops some browsers from redirecting.
-        }
-        // Don't do anything if dropping the same column we're dragging.
-        if(dragSrcEl != this) {
-          // Set the source column's HTML to the HTML of the column we dropped on.
-          var oldEl = { html: this.innerHTML, id: this.getAttribute('id')};
-          var newEl = { html: dragSrcEl.innerHTML, id: dragSrcEl.getAttribute('id')};
-          // swap all the data
-          dragSrcEl.innerHTML = oldEl.html;
-          dragSrcEl.setAttribute('id', oldEl.id);
-          this.innerHTML = newEl.html;
-          this.setAttribute('id', newEl.id);
-          if(settings.dropAnimation) {
-            onAnimEnd(this);
-            onAnimEnd(dragSrcEl);
-          }
-          settings.dropComplete();
-        }
-        return false;
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault(); // Necessary. Allows us to drop.
       }
+      e.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
+      return false;
+    }
+
+    function handleDrop(e) {
+      // this / e.target is current target element.
+      if (e.stopPropagation) {
+        e.stopPropagation(); // Stops some browsers from redirecting.
+      }
+      // Don't do anything if dropping the same column we're draggi.
+      if (dragSrcEl != this) {
+        // Set the source column's HTML to the HTML of the column wdropped on.
+        var oldEl = {
+          html: this.innerHTML,
+          id: this.id
+        };
+        var newEl = {
+          html: dragSrcEl.innerHTML,
+          id: dragSrcEl.id
+        };
+        // swap all the data
+        dragSrcEl.innerHTML = oldEl.html;
+        dragSrcEl.id = oldEl.id;
+        this.innerHTML = newEl.html;
+        this.id = newEl.id;
+        if (settings.dropAnimation) {
+          onAnimEnd(this);
+          onAnimEnd(dragSrcEl);
+        }
+        settings.dropComplete();
+      }
+      return false;
+    }
+
+    var settings = $.extend({}, this.defaultOptions, options);
+    var method = String(options);
+    var items = [];
+    // check for the methods
+    if (/^(toArray|toJSON)$/.test(method)) {
+      if (method == 'toArray') {
+        $(this).find(settings.element).each(function(index, elem) {
+          items.push(this.id);
+        });
+        return items;
+      } else if (method == 'toJSON') {
+        $(this).find(settings.element).each(function(index, elem) {
+          items[index] = {
+            id: this.id
+          };
+        });
+        return JSON.stringify(items);
+      }
+      return;
+    }
+    return this.each(function(index, item) {
+      var $this = $(this);
+      // select all but the disabled things
+      var $elem = $this.find(settings.element).filter(':not(' + settings.exclude + ')');
 
       function handleDragEnd(e) {
-        $(this).removeClass('moving');
+        $this.removeClass(settings.dropClass);
         // this/e.target is the source node.
         $elem.each(function(index, item) {
           $(this).removeClass(settings.overClass);
@@ -136,5 +147,5 @@
       $elem.on('drop', handleDrop);
       $elem.on('dragend', handleDragEnd);
     });
-};
+  };
 })(Zepto);
